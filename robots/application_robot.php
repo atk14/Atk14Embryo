858de1ec59811a53a2d986ddd9b4b2fe0c098309
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-10-21 10:24:58 +0200

    Robots updated

--- robots/application_robot.php
+++ robots/application_robot.php
@@ -3,13 +3,22 @@
  * The base class for every other robot.
  */
 class ApplicationRobot extends Atk14Robot{
+
 	function beforeRun(){
+		if(TEST){ return; }
 		$this->dbmole->begin(array(
 			"execute_after_connecting" => true
 		));
 	}
 
 	function afterRun(){
+		if(TEST){ return; }
 		$this->dbmole->commit();
+	}
+
+	function _commit(){
+		if(TEST){ return; }
+		$this->dbmole->commit();
+		$this->dbmole->begin();
 	}
 }
--- robots/reindex_database_robot.php
+++ robots/reindex_database_robot.php
@@ -3,10 +3,46 @@
  * In the current database reindexes all tables and materialized views belonging to the currently connected user
  *
  * It may be useful to run it daily or weekly.
+ *
+ * To reindex all table in the current schema:
+ *
+ *	./scripts/robot_runner reindex_database
+ *	# or
+ *	./scripts/robot_runner --robot=reindex_database
+ *
+ * To reindex table with the specific name and next tables in alphabetical order:
+ *
+ *	./scripts/robot_runner --robot=reindex_database --start-with-table=users
+ *	# or
+ *	./scripts/robot_runner --robot=reindex_database -susers
  */
 class ReindexDatabaseRobot extends ApplicationRobot {
 
 	function run(){
+		$arguments = getopt("s::",array("start-with-table::"));
+		$arguments += array(
+			"s" => null,
+		);
+		$arguments += array(
+			"start-with-table" => $arguments["s"],
+		);
+
+		$schema = Atk14Migration::GetDatabaseSchema();
+		$bind_ar = array(
+			":schema" => $schema
+		);
+
+		$this->dbmole->setErrorHandler(function($_dbmole){
+			throw new Exception("DbMoleException: ".$_dbmole->getErrorMessage());
+		});
+
+		$start_with_table = $arguments["start-with-table"];
+		$start_with_table_q = "";
+		if(strtolower($start_with_table)){
+			$start_with_table_q = " WHERE q.tablename>=:start_with_table";
+			$bind_ar[":start_with_table"] = "$schema.$start_with_table";
+		}
+
 		$tables = $this->dbmole->selectIntoArray("
 			SELECT tablename FROM (
 				SELECT
@@ -15,7 +51,8 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 					pg_tables
 				WHERE
 					hasindexes AND
-					tableowner=(SELECT CURRENT_USER)
+					tableowner=(SELECT CURRENT_USER) AND
+					schemaname=:schema
 				UNION
 				SELECT
 					schemaname||'.'||matviewname AS tablename
@@ -24,12 +61,42 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 				WHERE
 					hasindexes AND
 					matviewowner=(SELECT CURRENT_USER)
-			)q ORDER BY tablename
-		");
+			)q$start_with_table_q ORDER BY tablename
+		",$bind_ar);
+
 		foreach($tables as $table){
-			$this->dbmole->doQuery("REINDEX TABLE $table");
-			$this->logger->info("table $table reindexed");
-			$this->logger->flush();
+			$retries = 3;
+			while(1){
+				$exception_thrown = false;
+				try {
+					$this->dbmole->doQuery("REINDEX TABLE $table");
+				}catch(Exception $e){
+					$exception_thrown = true;
+					$message = $e->getMessage();
+					if(preg_match('/deadlock detected/',$message)){
+						$this->logger->info("deadlock detected while reindexing $table");
+						$this->logger->flush();
+					}else{
+						$this->logger->error($message);
+						$this->logger->flush();
+						return;
+					}
+				}
+
+				if(!$exception_thrown){
+					$this->logger->info("table $table reindexed");
+					$this->logger->flush();
+					break;
+				}
+
+				if($retries<=0){
+					$this->logger->info("table $table was NOT reindexed, exiting...");
+					exit; // exit and not return! we don't want the robot lock file to be deleted.
+				}
+
+				sleep(5);
+				$retries--;
+			}
 		}
 	}
 }
